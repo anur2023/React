d78a0fc1bcdc0beda5c30afcf801c115f1672c76
Anuruddh <anuruddh209401@gmail.com>
2026-02-05 23:01:51 +0530

    Smart Counter Counter

---
+++ Hook1_Task/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import SmartCounterDashboard from './Components/SmartCounterDashboard'
+
+function App() {
+  return (
+    <>
+      <SmartCounterDashboard />
+    </>
+  )
+}
+
+export default App
+
---
+++ Hook1_Task/src/Components/SmartCounterDashboard.jsx
@@ -0,0 +1,49 @@
+
+
+import {useState} from 'react';
+
+
+function SmartCounterDashboard() {
+  const [counter, setCount] = useState(0)
+  function addValue(){
+    // Apply a logic in whcih the counter value can't be greater than 20.
+    if(counter<20)
+    {
+     
+      setCount(counter+1)
+    }
+    else{
+       alert("Count can't exceed to 20.")
+    }
+  }
+    function removeValue(){
+      // Apply a logic in whcih the counter value can't be less than 0.
+      if(counter >0)
+    {
+      setCount(counter-1)
+    }
+    else{
+      alert("You can't reduce the value after 0.")
+    }
+  }
+  function reset(){
+    setCount(0);
+  }
+  return (
+    <>
+    <h1>This is a counter project</h1>
+    <h2>
+      Current value of counter : {counter};
+    </h2>
+    <button onClick={addValue}>Increase count {counter}</button>
+    <br />
+    <br />
+    <button onClick={removeValue}>Decrease Value {counter}</button>
+    <br /><br />
+    <button onClick={reset}>Reset Counter</button>
+    </>
+  )
+}
+
+
+export default  SmartCounterDashboard;
